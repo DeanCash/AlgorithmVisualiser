32e65fe86a2d963f644e4644a441d006b0c8100f
DeanCash <72015294+DeanCash@users.noreply.github.com>
2023-12-15 15:56:00 +0100

    Update generate_dataset.py

--- generate_dataset.py
+++ generate_dataset.py
@@ -2,26 +2,13 @@
 import random
 
 file = "data.csv"
+
 set_size = 1000
 
 biggest = 500
 smallest = 0
 
-dataset = ""
-
-for i in range(set_size):
-    dataset += str(random.randint(smallest, biggest))
-    if i != set_size - 1:
-        dataset += ","
+dataset = ",".join([str(random.randint(smallest, biggest)) for _ in range(set_size)])
 
 with open(file, "w") as f:
     f.write(dataset)
-
-
-
-
-
-
-
-
-
